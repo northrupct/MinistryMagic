8d738cd9ec1f866d292a51c9aab9b74003f7bca4
Carey Northrup <carey.t.northrup@boeing.com>
2018-06-04 15:43:09 -0600

    AboutDlg

--- src/com/blackops/view/AboutDlg.java
+++ src/com/blackops/view/AboutDlg.java
@@ -17,18 +17,6 @@ public class AboutDlg extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		try {
-			AboutDlg dialog = new AboutDlg();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 
 	/**
 	 * Create the dialog.
